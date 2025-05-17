735d8db2f61ac99e5260008999de4dd234aa30cc
alexandrappe <00117222@uca.edu.sv>
2025-05-17 17:58:05 -0600

    Add student entity

---
+++ exercise/src/main/java/com/lab02/exercise/entities/Student.java
@@ -0,0 +1,47 @@
+package com.lab02.exercise.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "student")
+public class Student{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    //Nombre
+    @Column(nullable = false)
+    private String firstName;
+
+    //Apellido
+    @Column(nullable = false)
+    private String lastName;
+
+    //Prestamo
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Loan> loans;
+
+    //Libro
+
+
+    //Constructor
+    public Student() {}
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //Getters y Setters
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public List<Loan> getLoans() { return loans; }
+    public void setLoans(List<Loan> loans) { this.loans = loans; }
+}
+
