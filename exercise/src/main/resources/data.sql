983078e2cc64c19a5a4377cbd6d557139a6efa6a
alexandrappe <00117222@uca.edu.sv>
2025-05-18 00:32:09 -0600

    Add data.sql file

---
+++ exercise/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO students (firstName, lastName, carnet, email) VALUES ('Jose', 'Jacobo', '00243522', '00243522@uca.edu.sv');
+INSERT INTO students (firstName, lastName,  carnet, email) VALUES ('Paola', 'Perez', '00117222', '00117222@uca.edu.sv');
+INSERT INTO students (firstName, lastName,  carnet, email) VALUES ('Jose', 'Ramírez', '00989821', '00989821@uca.edu.sv');
+INSERT INTO students (firstName, lastName, carnet, email) VALUES ('Luis', 'Hernández', '00678901', '00678921@uca.edu.sv');
+INSERT INTO students (firstName, lastName, carnet, email) VALUES ('Sofía', 'Castro', '00789012', '00789012@uca.edu.sv');
+INSERT INTO students (firstName, lastName, carnet, email) VALUES ('Luis', 'Méndez', '00890123', '00890123@uca.edu.sv');
+INSERT INTO students (firstName, lastName, carnet, email) VALUES ('Andrea', 'Perez', '00901234', '00901214@uca.edu.sv');
